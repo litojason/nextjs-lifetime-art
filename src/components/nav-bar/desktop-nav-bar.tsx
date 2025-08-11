37cf61015cff5011b00be5044ae0f576630d5a20
Jason <litojason25@gmail.com>
2025-08-11 22:59:34 +0700

    refactor(folder-structure): refactor nav-bar related components.

--- src/components/nav-bar/desktop-nav-bar.tsx
+++ src/components/nav-bar/desktop-nav-bar.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import Logo from "./logo";
+import Logo from "../logo";
 
 type DesktopNavBarProps = {
   menus: string[];
--- src/components/nav-bar/mobile-nav-bar.tsx
+++ src/components/nav-bar/mobile-nav-bar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 
-import Logo from "./logo";
+import Logo from "../logo";
 
 type MobileNavBarProps = {
   menus: string[];
